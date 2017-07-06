da1f9040ab8a99f4e5ad1e9140c746b89d4ccab9
lnmnd <lnmnd@users.noreply.github.com>
2017-07-06 20:34:01 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='pyccup',
+    version='0.1.0',
+    description='Library for representing HTML in Python',
+    url='https://github.com/lnmnd/pyccup',
+    author='lnmnd',
+    author_email='lnmnd@users.noreply.github.com',
+    license='MIT',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    keywords='html hiccup',
+    py_modules=['pyccup']
+)
